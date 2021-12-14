ee18ab55cb8c36f2638c50a624cf7e23735eb26d
Kris Dreher <k.dreher@dkfz-heidelberg.de>
2021-12-14 17:49:36 +0100

    Changed source order

--- src/mcx_utils.c
+++ src/mcx_utils.c
@@ -189,7 +189,8 @@ const char boundarydetflag[]={'0','1','\0'};
 
 const char *srctypeid[]={"pencil","isotropic","cone","gaussian","planar",
     "pattern","fourier","arcsine","disk","fourierx","fourierx2d","zgaussian",
-    "line","slit","pencilarray","pattern3d","hyperboloid","invision", "msot_acuity_echo", ""};
+    "line","slit","pencilarray","pattern3d","hyperboloid","msot_acuity_echo",
+    "invision", ""};
 
 
 /**
